7a54a9a79ff160f0dd8bc3245be160304561aecd
LeoKliemann <lgkliemann05@gmail.com>
2026-02-24 09:11:32 -0500

    Finished Task 1

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -4,6 +4,29 @@ public class CtoFConverter{
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        
+        double celsius = 0;
+        double fahrenheit;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print("Enter the degrees Celsius: ");
+
+            if (in.hasNextDouble()) {
+                celsius = in.nextDouble();
+                in.nextLine();
+
+                fahrenheit = (celsius * 1.8) + 32;
+
+                System.out.println("The degrees Fahrenheit is: " + fahrenheit);
+
+                done = true;
+            } else {
+
+                trash = in.nextLine();
+                System.out.println("\nYou said the temperature was: " + trash);
+                System.out.println("Please enter a valid amount.");
+            }
+        }while(!done);
     }
 }
